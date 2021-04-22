fdb7434f6ee3a94d235eb8fe9492bdaecae29329
substantialdarkness <82995264+substantialdarkness@users.noreply.github.com>
2021-04-22 13:23:19 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=185cCwiN1flqpILlnbDkDcL1ccHsgGHUI'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Trump', 'Biden', 'Kamala','Pence']
 path = Path(__file__).parent
 
 app = Starlette()
